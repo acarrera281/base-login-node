3f15a459e616aab7cf73ab208aa14d41ca3d64da
Angel Carrera <angelcarrera91@gmail.com>
2019-01-22 12:31:36 -0300

    make it better parte 4 con mlab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//'url': 'mongodb://localhost/nodelogin'
-	'url': process.env.MONGODB 
+	'url': 'mongodb://admin:acarrera281@ds029735.mlab.com:29735/login-node'
+	//'url': process.env.MONGODB 
 };
